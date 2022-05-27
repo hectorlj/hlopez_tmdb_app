a6fecdf6f13cd0fbb12a31fc2f001890ef872d03
Hector Lopez <hector.lopez@strakertranslations.com>
2022-05-27 15:23:14 -0600

    updated express app to align with requirements

--- webservice/api/routes/movies.js
+++ webservice/api/routes/movies.js
@@ -2,6 +2,6 @@ import express from 'express';
 import * as movieController from '../controller/movieController.js';
 const router = express.Router();
 
-router.get("/searchMovies", movieController.getters.searchMovies)
+router.get("/movies", movieController.getters.searchMovies)
 
 export default router;
--- webservice/app.js
+++ webservice/app.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 const app = express();
 app.use(cors());
 import movieRoutes from'./api/routes/movies.js';
-app.use("/movies", movieRoutes);
+app.use("/imdb", movieRoutes);
 
 app.listen(5001, () => {
 	console.log('Listening on port 5001');
