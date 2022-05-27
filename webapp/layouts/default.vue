d95fb3ae6d538961414486d3e150c30deedb6afc
Hector Lopez <hector.lopez@strakertranslations.com>
2022-05-27 15:27:14 -0600

    updated the navbar and searchResult component imports to use methods, handle emits, and display properly

--- webapp/layouts/default.vue
+++ webapp/layouts/default.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
-        <navbar />
-        <searchResults />
+        <navbar
+            @search="searchInput"
+        />
+        <searchResults
+            v-if="showResults"
+            :results="results"
+            :searchInput="searchInput"
+            @clearResults="clearResults()"
+        />
         <nuxt />
     </div>
 </template>
@@ -12,6 +19,32 @@ export default {
     components: {
         navbar,
         searchResults
+    },
+    data() {
+        return {
+            results: []
+        }
+    },
+    methods: {
+        searchInput(value) {
+            this.$axios.$get(`/imdb/movies?search=${value}`)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+                this.results = [] 
+            })
+        },
+        clearResults() {
+            this.results = []
+        }
+    },
+    computed: {
+        showResults() {
+            return this.results.length > 0 ? true : false;
+        },
+
     }
 }
 </script>
